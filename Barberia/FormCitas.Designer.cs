49373a3f349920e8c7e0606a3fd523a587c25d14
mauarredondos <arredondosmauricio@gmail.com>
2025-12-02 15:58:51 -0600

    solucion de errores
    habia un label que no dejaba pulsar un boton

--- Barberia/FormCitas.Designer.cs
+++ Barberia/FormCitas.Designer.cs
@@ -41,99 +41,75 @@ namespace Barberia
             this.dgvCitas = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCitas)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
+            
             this.label1.Location = new System.Drawing.Point(20, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(75, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Cliente:";
-            // 
-            // label2
-            // 
+             
             this.label2.Location = new System.Drawing.Point(7, 57);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Fecha y hora:";
-            // 
-            // label3
-            // 
+             
             this.label3.Location = new System.Drawing.Point(20, 100);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(75, 23);
             this.label3.TabIndex = 2;
             this.label3.Text = "Servicio:";
-            // 
-            // label4
-            // 
+            
             this.label4.Location = new System.Drawing.Point(20, 140);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(75, 23);
             this.label4.TabIndex = 3;
             this.label4.Text = "Nota:";
-            // 
-            // cmbCliente
-            // 
+            
             this.cmbCliente.Location = new System.Drawing.Point(100, 18);
             this.cmbCliente.Name = "cmbCliente";
             this.cmbCliente.Size = new System.Drawing.Size(200, 24);
             this.cmbCliente.TabIndex = 4;
-            // 
-            // dtpFecha
-            // 
+            
             this.dtpFecha.CustomFormat = "dd/MM/yyyy HH:mm";
             this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dtpFecha.Location = new System.Drawing.Point(100, 58);
             this.dtpFecha.Name = "dtpFecha";
             this.dtpFecha.Size = new System.Drawing.Size(200, 22);
             this.dtpFecha.TabIndex = 5;
-            // 
-            // txtServicio
-            // 
+             
             this.txtServicio.Location = new System.Drawing.Point(100, 98);
             this.txtServicio.Name = "txtServicio";
             this.txtServicio.Size = new System.Drawing.Size(200, 22);
             this.txtServicio.TabIndex = 6;
-            // 
-            // txtNota
-            // 
+            
             this.txtNota.Location = new System.Drawing.Point(100, 138);
             this.txtNota.Multiline = true;
             this.txtNota.Name = "txtNota";
             this.txtNota.Size = new System.Drawing.Size(250, 80);
             this.txtNota.TabIndex = 7;
-            // 
-            // btnCrear
-            // 
+            
             this.btnCrear.Location = new System.Drawing.Point(20, 240);
             this.btnCrear.Name = "btnCrear";
             this.btnCrear.Size = new System.Drawing.Size(75, 23);
             this.btnCrear.TabIndex = 8;
             this.btnCrear.Text = "Crear";
             this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
-            // 
-            // btnEliminar
-            // 
+            
             this.btnEliminar.Location = new System.Drawing.Point(100, 240);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(75, 23);
             this.btnEliminar.TabIndex = 9;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
-            // 
-            // dgvCitas
-            // 
+            
             this.dgvCitas.ColumnHeadersHeight = 29;
             this.dgvCitas.Location = new System.Drawing.Point(20, 290);
             this.dgvCitas.Name = "dgvCitas";
             this.dgvCitas.RowHeadersWidth = 51;
             this.dgvCitas.Size = new System.Drawing.Size(500, 220);
             this.dgvCitas.TabIndex = 10;
-            // 
-            // FormCitas
-            // 
+         
             this.ClientSize = new System.Drawing.Size(639, 550);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
