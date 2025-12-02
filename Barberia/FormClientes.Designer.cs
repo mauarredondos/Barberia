c11f4e855d28553c368a48cc2b10488f9f0e0fc3
mauarredondos <arredondosmauricio@gmail.com>
2025-12-02 16:01:54 -0600

    .

--- Barberia/FormClientes.Designer.cs
+++ Barberia/FormClientes.Designer.cs
@@ -38,75 +38,57 @@ namespace Barberia
             this.txtId = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
+            
             this.label1.Location = new System.Drawing.Point(20, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(75, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre:";
-            // 
-            // label2
-            // 
+            
             this.label2.Location = new System.Drawing.Point(20, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Teléfono:";
-            // 
-            // txtNombre
-            // 
+            
             this.txtNombre.Location = new System.Drawing.Point(100, 21);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(200, 22);
             this.txtNombre.TabIndex = 2;
-            // 
-            // txtTelefono
-            // 
+            
             this.txtTelefono.Location = new System.Drawing.Point(100, 58);
             this.txtTelefono.Name = "txtTelefono";
             this.txtTelefono.Size = new System.Drawing.Size(150, 22);
             this.txtTelefono.TabIndex = 3;
-            // 
-            // btnAgregar
-            // 
+            
             this.btnAgregar.Location = new System.Drawing.Point(20, 100);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 4;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
-            // 
-            // btnEditar
-            // 
+            
             this.btnEditar.Location = new System.Drawing.Point(110, 100);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(75, 23);
             this.btnEditar.TabIndex = 5;
             this.btnEditar.Text = "Editar";
             this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
-            // 
-            // btnEliminar
-            // 
+            
             this.btnEliminar.Location = new System.Drawing.Point(200, 100);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(75, 23);
             this.btnEliminar.TabIndex = 6;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
-            // 
-            // btnLimpiar
-            // 
+            
             this.btnLimpiar.Location = new System.Drawing.Point(290, 100);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
             this.btnLimpiar.TabIndex = 7;
             this.btnLimpiar.Text = "Limpiar";
             this.btnLimpiar.Visible = false;
-            // 
-            // dgvClientes
-            // 
+            
             this.dgvClientes.ColumnHeadersHeight = 29;
             this.dgvClientes.Location = new System.Drawing.Point(20, 150);
             this.dgvClientes.Name = "dgvClientes";
@@ -114,18 +96,14 @@ namespace Barberia
             this.dgvClientes.Size = new System.Drawing.Size(500, 250);
             this.dgvClientes.TabIndex = 8;
             this.dgvClientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClientes_CellContentClick);
-            // 
-            // txtId
-            // 
+            
             this.txtId.Location = new System.Drawing.Point(100, 129);
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(100, 22);
             this.txtId.TabIndex = 9;
             this.txtId.Visible = false;
             this.txtId.VisibleChanged += new System.EventHandler(this.False);
-            // 
-            // FormClientes
-            // 
+             
             this.ClientSize = new System.Drawing.Size(600, 450);
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.label1);
