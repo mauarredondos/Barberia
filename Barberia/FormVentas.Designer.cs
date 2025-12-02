5a1ef4569e944fc01e9f84cb3358f47c68efa2e4
mauarredondos <arredondosmauricio@gmail.com>
2025-12-02 16:04:10 -0600

    .

--- Barberia/FormVentas.Designer.cs
+++ Barberia/FormVentas.Designer.cs
@@ -38,48 +38,36 @@ namespace Barberia
             ((System.ComponentModel.ISupportInitialize)(this.numPrecio)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvVentas)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
+             
             this.label1.Location = new System.Drawing.Point(20, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(74, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Cliente:";
-            // 
-            // label2
-            // 
+            
             this.label2.Location = new System.Drawing.Point(20, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Servicio:";
-            // 
-            // label3
-            // 
+            
             this.label3.Location = new System.Drawing.Point(20, 100);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(74, 23);
             this.label3.TabIndex = 2;
             this.label3.Text = "Precio:";
-            // 
-            // cmbCliente
-            // 
+            
             this.cmbCliente.Location = new System.Drawing.Point(100, 18);
             this.cmbCliente.Name = "cmbCliente";
             this.cmbCliente.Size = new System.Drawing.Size(200, 24);
             this.cmbCliente.TabIndex = 3;
             this.cmbCliente.SelectedIndexChanged += new System.EventHandler(this.cmbCliente_SelectedIndexChanged);
-            // 
-            // txtServicio
-            // 
+             
             this.txtServicio.Location = new System.Drawing.Point(100, 58);
             this.txtServicio.Name = "txtServicio";
             this.txtServicio.Size = new System.Drawing.Size(200, 22);
             this.txtServicio.TabIndex = 4;
-            // 
-            // numPrecio
-            // 
+             
             this.numPrecio.DecimalPlaces = 2;
             this.numPrecio.Location = new System.Drawing.Point(100, 98);
             this.numPrecio.Maximum = new decimal(new int[] {
@@ -90,36 +78,28 @@ namespace Barberia
             this.numPrecio.Name = "numPrecio";
             this.numPrecio.Size = new System.Drawing.Size(120, 22);
             this.numPrecio.TabIndex = 5;
-            // 
-            // btnRegistrar
-            // 
+            
             this.btnRegistrar.Location = new System.Drawing.Point(20, 140);
             this.btnRegistrar.Name = "btnRegistrar";
             this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
             this.btnRegistrar.TabIndex = 6;
             this.btnRegistrar.Text = "Registrar";
             this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
-            // 
-            // btnEliminar
-            // 
+             
             this.btnEliminar.Location = new System.Drawing.Point(120, 140);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(75, 23);
             this.btnEliminar.TabIndex = 7;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
-            // 
-            // dgvVentas
-            // 
+            
             this.dgvVentas.ColumnHeadersHeight = 29;
             this.dgvVentas.Location = new System.Drawing.Point(20, 190);
             this.dgvVentas.Name = "dgvVentas";
             this.dgvVentas.RowHeadersWidth = 51;
             this.dgvVentas.Size = new System.Drawing.Size(500, 250);
             this.dgvVentas.TabIndex = 8;
-            // 
-            // FormVentas
-            // 
+             
             this.ClientSize = new System.Drawing.Size(600, 480);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
